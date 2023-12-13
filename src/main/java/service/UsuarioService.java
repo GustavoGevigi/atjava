c904ad9f18bc9fa9e4a7ea9aa28b8782ebf659e9
GustavoGevigi <136391922+GustavoGevigi@users.noreply.github.com>
2023-12-12 22:11:21 -0300

    UsuarioService.java

--- src/main/java/service/UsuarioService.java
+++ src/main/java/service/UsuarioService.java
@@ -19,20 +19,17 @@ public class UsuarioService {
         this.modelMapper = new ModelMapper();
     }
 
-    // Método para listar todos os usuários
     public List<UsuarioDTOOutput> listarUsuarios() {
         return listaUsuarios.stream()
                 .map(usuario -> modelMapper.map(usuario, UsuarioDTOOutput.class))
                 .collect(Collectors.toList());
     }
 
-    // Método para inserir um novo usuário a partir de um objeto UsuarioDTOInput
     public void inserirUsuario(UsuarioDTOInput usuarioDTOInput) {
         Usuario usuario = modelMapper.map(usuarioDTOInput, Usuario.class);
         listaUsuarios.add(usuario);
     }
 
-    // Método para alterar um usuário existente a partir de um objeto UsuarioDTOInput
     public void alterarUsuario(UsuarioDTOInput usuarioDTOInput) {
         Optional<Usuario> usuarioExistente = listaUsuarios.stream()
                 .filter(u -> u.getId() == usuarioDTOInput.getId())
@@ -40,30 +37,21 @@ public class UsuarioService {
 
         usuarioExistente.ifPresent(u -> {
             modelMapper.map(usuarioDTOInput, u);
-            // Alternativamente, você poderia mapear manualmente os campos, por exemplo:
-            // u.setNome(usuarioDTOInput.getNome());
-            // u.setSenha(usuarioDTOInput.getSenha());
         });
     }
 
-    // Método para atualizar um usuário a partir de um objeto UsuarioDTOInput
-// Método para atualizar um usuário a partir de um objeto UsuarioDTOInput
     public void atualizarUsuario(UsuarioDTOInput usuarioDTO) {
-        // Verificar se o usuário com o ID fornecido existe na lista
         UsuarioDTOOutput usuarioExistente = buscarUsuarioPorId(usuarioDTO.getId());
 
         if (usuarioExistente != null) {
-            // Atualizar os dados do usuário existente com base no DTO de entrada
             usuarioExistente.setId(usuarioDTO.getId());
             usuarioExistente.setNome(usuarioDTO.getNome());
             usuarioExistente.setSenha(usuarioDTO.getSenha());
         } else {
-            // Se o usuário não existe, pode lançar uma exceção ou tratar conforme necessário
             throw new RuntimeException("Usuário não encontrado para atualização.");
         }
     }
 
-    // Método para buscar um usuário por ID e converter para UsuarioDTOOutput
     public UsuarioDTOOutput buscarUsuarioPorId(int id) {
         Optional<Usuario> usuarioExistente = listaUsuarios.stream()
                 .filter(u -> u.getId() == id)
@@ -73,7 +61,6 @@ public class UsuarioService {
                 .orElse(null);
     }
 
-    // Método para excluir um usuário por ID
     public void excluirUsuario(int id) {
         listaUsuarios.removeIf(u -> u.getId() == id);
     }

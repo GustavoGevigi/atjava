754d194d159fcd58e79a1f88088812f20d7c88af
GustavoGevigi <136391922+GustavoGevigi@users.noreply.github.com>
2023-12-12 22:10:25 -0300

    ServiceTest.java

--- src/main/java/service/ServiceTest.java
+++ src/main/java/service/ServiceTest.java
@@ -18,15 +18,12 @@ public class ServiceTest {
 
     @Test
     public void testInserirUsuario() {
-        // Criar um objeto UsuarioDTOInput para inserção
         UsuarioDTOInput novoUsuarioDTO = new UsuarioDTOInput();
         novoUsuarioDTO.setNome("Novo Usuário");
         novoUsuarioDTO.setSenha("senha123");
 
-        // Executar o método inserir
         usuarioService.inserirUsuario(novoUsuarioDTO);
 
-        // Validar a inserção verificando o tamanho da lista após a inserção
         assertEquals(1, usuarioService.listarUsuarios().size());
     }
 }
